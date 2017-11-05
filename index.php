d6c22ca907deca0ce73198a244a03bd68e60151e
Aakashkumar Bhagat <ab997@afsconnect1.njit.edu>
2017-11-05 16:08:23 -0500

    date and color

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 	<?php
 
-
+        echo"<center> <h1> <u> WEEK-7 ASSIGNMENT </u> </h1> </center>";
+        echo "<body style='background-color:#B0C4DE'>";
+        echo  "<center> <b>Attn: Prof. Keith Willams </b> <br> </center> ";
+        echo "<center> <b>TA: Ikjyot Singh Gujral </b><br></center> ";
+        echo "<center> <b> TA: Brandon Major </b> <br ></ center>";
+        $a= date('m-d-y',time()) ;
+        echo "<center><b> Date: $a </b> <br>  </center>";
+        ini_set('display_errors', 'On');
+        error_reporting(E_ALL);
+        echo '<br>';
 	$dsn = 'mysql:dbname=ab997;host=sql1.njit.edu';
 	$user = 'ab997';
 	$password = 'pKFturo1';
